ba627bec83e4ba6632f6ebc0e186eb44b7dddc5f
Z <zdenek@me.com>
2013-07-05 22:14:30 +0200

    Dummy snowcrash C++/CLI wrapper

--- snowcrashCLR.h
+++ snowcrashCLR.h
@@ -1,13 +1,25 @@
 // snowcrashCLR.h
 
 #pragma once
+#include "snowcrash\src\snowcrash.h"
 
 using namespace System;
 
 namespace snowcrashCLR {
 
+	// TODO: Rename
 	public ref class Class1
 	{
 		// TODO: Add your methods for this class here.
+
+	public:
+
+		// TODO: Rename & revisit
+		static void test()
+		{
+			snowcrash::Result result;
+			snowcrash::Blueprint blueprint;
+			snowcrash::parse("# My API\n", 0, result, blueprint);
+		}
 	};
 }
