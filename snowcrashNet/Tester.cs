b2c7e586576b3181295d5693a40d5555441716f9
SeriousM <masta2ooo@gmail.com>
2013-07-06 08:54:02 +0200

    Added the .net tester project;

---
+++ snowcrashNet/Tester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snowcrashNet
+{
+    public class Tester
+    {
+        public void test()
+        {
+            snowcrashCLR.Tester.test();
+        }
+    }
+}
